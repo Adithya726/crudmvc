e22241c26a52e90cfa2851f8a9c1733909b6b860
Adithya726 <163718794+Adithya726@users.noreply.github.com>
2024-11-11 23:47:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
+// Load environment variables from .env file
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const studentRoutes = require('./routes/studentRoutes');
 
-dotenv.config();
-
 const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+const corsOptions = {
+  origin: 'http://localhost:5000/api/students',
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
 
+// Connect to MongoDB using the connection string from environment variables
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
+// Use routes for API
 app.use('/api', studentRoutes);
 
+// Start the server on the specified port or default to 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
